3b264e577ea18b33816393f7cf80f52e747f366a
root <IvanBalashov>
2018-07-29 14:29:26 +0200

    start writing target download

--- bot.py
+++ bot.py
@@ -11,53 +11,57 @@ from database import SQLighter
 bot = telebot.TeleBot(bot_token)
 db = SQLighter(database_name)
 
-@bot.message_handler(commands = ['start'])
+@bot.message_handler(commands=['start'])
 def start(message):
     message.text = f"Привет. Я простой бот который может скачать твой любимый трек с youtube. Для того чтоб научиться мной пользоваться \
         введи /help."
     bot.send_message(message.chat.id, message.text)
 
-@bot.message_handler(commands = ['help'])
+@bot.message_handler(commands=['help'])
 def start(message):
     message.text = f"На самом деле все очень просто пока что. Надо всего лишь.... отправить мне ссылку на видео из которого ты хочешь достать \
         аудиодорожку и ждать) да, пока что сервис работает не очень быстро, но в дальнейшем разработчик все поправит"
     bot.send_message(message.chat.id, message.text)
 
-@bot.message_handler(content_types = ["text"])
-def get_music(message):
-    i = 2
-    url = re.findall('https://www.youtube.com/watch\?v\=[0-9A-Za-z\_\-]{11}|https://youtu.be/[0-9A-Za-z\_\-]{11}',message.text)
+@bot.message_handler(content_types=["text"])
+def get_music(message: str):
+    url = re.findall('https://www.youtube.com/watch\?v\=[0-9A-Za-z\_\-]{11}|https://youtu.be/[0-9A-Za-z\_\-]{11}', message.text)
     if len(url) == 0:
         message.text = f"Only youtube URLs."
         bot.send_message(message.chat.id, message.text)
     else:
-        url = message.text  
+        start: list = re.findall('\s(-старт|-с|-start|-s)\s(\d{1,2}|\d{1,2}.\d{1,2})', message.text)
+        finish: list = re.findall('\s(-конец|-к|-end|-e)\s(\d{1,2}|\d{1,2}.\d{1,2})', message.text)
+        if len(start) != 0:
+            print(start)
+        if len(finish) != 0:
+            print(finish)
+        url = message.text
         if db.check_exist_file(url):
             fileid = db.select_by_url(url)
             bot.send_audio(message.chat.id, fileid[0], None, timeout = 5)
         else:
             t1 = bot.send_message(message.chat.id, f"0%")
-            bot.edit_message_text(f"25%", chat_id = message.chat.id, message_id = t1.message_id)
+            bot.edit_message_text(f"25%", chat_id=message.chat.id, message_id=t1.message_id)
             fake_name, title = eng.download_by_link(url, message.chat.id)
-            bot.edit_message_text(f"50%", chat_id = message.chat.id, message_id = t1.message_id) 
+            bot.edit_message_text(f"50%", chat_id=message.chat.id, message_id=t1.message_id) 
             path = eng.convert_to_mp3(fake_name, title)
             size = getsize(path) / 1024 / 1024
             if size > 30:
-                bot.edit_message_text(f"Can't load this song.", chat_id = message.chat.id, message_id = t1.message_id)
+                bot.edit_message_text(f"Can't load this song.", chat_id=message.chat.id, message_id=t1.message_id)
             else:
                 f = open(path, 'rb')
-                bot.edit_message_text(f"75%", chat_id = message.chat.id, message_id = t1.message_id)
+                bot.edit_message_text(f"75%", chat_id=message.chat.id, message_id=t1.message_id)
                 try:
                     msg = bot.send_audio(message.chat.id, f, None, timeout = 60)
-                    bot.edit_message_text(f"100%", chat_id = message.chat.id, message_id = t1.message_id)
+                    bot.edit_message_text(f"100%", chat_id=message.chat.id, message_id=t1.message_id)
                     db.add_file(file_id = msg.audio.file_id, file_name = "0", url = url)
                     pass
                 except Exception:
-                    bot.edit_message_text(f"TimeOut.", chat_id = message.chat.id, message_id = t1.message_id)
+                    bot.edit_message_text(f"TimeOut.", chat_id=message.chat.id, message_id=t1.message_id)
                     pass
                 eng.remove_file(path)
     time.sleep(3)
 
 if __name__ == '__main__':
     bot.polling(none_stop = True)
-
--- eng.py
+++ eng.py
@@ -7,7 +7,8 @@ from os import listdir, remove, devnull
 from os.path import isfile, join
 from data.config import path_to_wrk_dir
 
-def download_by_link(link, videoid):
+def download_by_link(link: str, videoid: str, start: int=None, end: int=None) -> str, str:
+	"""This method is setup youtube_dl for downlad video"""
 	ydl_opts = {
 		'quiet': True,
 		'no_warnings': True,
@@ -18,6 +19,11 @@ def download_by_link(link, videoid):
 			'preferredquality': '512',
 		 }],
 	}
+	if start is not None:
+		ydl_opts['start_time'] = start
+	if end is not None:
+		ydl_opts['end_time'] = end
+	print(f"ydl_opts = {ydl_opts}\n")
 	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
 		data = ydl.extract_info(link)
 	fake_name = 'NA' + str(videoid)
@@ -27,15 +33,16 @@ def download_by_link(link, videoid):
 	title = title.replace(' ', '_')
 	return fake_name, title
 
-def translate(inp):
+def translate(inp: str) -> str:
+	"""This is strange method for retranslate cirylic symbols in latin.
+	   Stolen from stackoverflow)."""
 	symbols =(u"абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ",
 			u"abvgdeejzijklmnoprstufhzcss_y_euaABVGDEEJZIJKLMNOPRSTUFHZCSS_Y_EUA")
 	tr = {ord(a):ord(b) for a, b in zip(*symbols)}
 	output = inp.translate(tr)
 	return output
 
-def convert_to_mp3(filename, title):
-	DEVNULL = open(devnull, 'wb')
+def convert_to_mp3(filename: str, title: str) -> str:
 	fileB = f"{path_to_wrk_dir}{title}.mp3"
 	meta = f"-metadata"
 	newtitle = f"title={title}"
@@ -53,17 +60,15 @@ def convert_to_mp3(filename, title):
 		remove(fileA)
 	except FileNotFoundError:
 		files = get_file_list(path_to_wrk_dir)
-		print(files)
 		for i in files:
 			if -1 != f"{path_to_wrk_dir}{i}".find(f"{filename}") and f"{i}".find(f".mp3") == -1:
-				print(f"removed - {path_to_wrk_dir}{i}")
 				try:
 					remove(f"{path_to_wrk_dir}{i}")
 				except FileNotFoundError:
 					print(f"can't remove file {path_to_wrk_dir}{i}")
 	return fileB
 
-def remove_file(path):
+def remove_file(path: str) -> None:
 	remove(path)
 
 def get_file_list(path: str) -> list:
